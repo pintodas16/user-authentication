5703bc423d6330b8a8d174a68fa07363f51a9690
pinto <pintodas.lu@gmail.com>
2026-05-27 04:37:41 +0200

    cors issue

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://user-authentication-client-mu.vercel.app/', // ✅ replace with your actual frontend URL
+    'https://user-authentication-client-mu.vercel.app', // ✅ replace with your actual frontend URL
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type','Authorization']
